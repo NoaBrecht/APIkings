5355b0e904ec67ac931e1247b0dc6aef8c1e0c80
NoaBrecht <99073951+NoaBrecht@users.noreply.github.com>
2024-04-04 08:17:20 +0000

    Update background color and image in CSS and HTML files

--- index.ts
+++ index.ts
@@ -13,11 +13,50 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set('views', path.join(__dirname, "views"));
 
 app.set("port", process.env.PORT || 3000);
-
-app.get("/", (req, res) => {
-    res.render("index", {
-        title: "Alle Pokémon bekijken"
-    })
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+});
+app.get("/", async (req, res) => {
+    try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+        if (response.status === 404) throw new Error('Not found');
+        if (response.status === 500) throw new Error('Internal server error');
+        if (response.status === 400) throw new Error('Bad request');
+        let pokemons: any = await response.json();
+        const pokemonWithImages = await Promise.all(pokemons.results.map(async (pokemon: any) => {
+            const response = await fetch(pokemon.url);
+            const data = await response.json();
+            return {
+                id: data.id,
+                name: pokemon.name,
+                image: data.sprites.other.home.front_default,
+                type: data.types[0].type.name
+            };
+        }));
+        res.render('index', {
+            title: "Brands",
+            pokemons: pokemonWithImages
+        });
+    } catch (error) {
+        console.error('Error:', error);
+    }
+});
+app.get("/pokemon/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        if (response.status === 404) throw new Error('Not found');
+        if (response.status === 500) throw new Error('Internal server error');
+        if (response.status === 400) throw new Error('Bad request');
+        const pokemon = await response.json();
+        res.render('pokemon', {
+            title: pokemon.name,
+            pokemon: pokemon
+        });
+    } catch (error) {
+        console.error('Error:', error);
+    }
 });
 
 app.listen(app.get("port"), () => {
