2ba0b11956ff0a53ea7b5dff93c3481cc9175ecc
NoaBrecht <99073951+NoaBrecht@users.noreply.github.com>
2024-05-15 18:43:11 +0000

    iets betere battle

--- functions.ts
+++ functions.ts
@@ -1,11 +1,7 @@
 export function calculateFight(hp: number, defence: number, attack: number) {
-    let damage: number = (hp - (defence - attack));
-    if (hp > 0) {
-        return 0
-    }
-    console.log(6)
-    console.log(`damage: ${damage}`);
-    return hp - damage;
+    let damage: number = (defence - attack);
+    console.debug(`damage: ${damage}`);
+    return hp + damage;
 }
 export function battle(pokemon1: any, pokemon2: any) {
     console.log(`${pokemon1.name} is fighting ${pokemon2.name}`);
@@ -15,20 +11,20 @@ export function battle(pokemon1: any, pokemon2: any) {
     pokemon2.attack = pokemon2.stats[1].base_stat;
     pokemon1.defense = pokemon1.stats[2].base_stat;
     pokemon2.defense = pokemon2.stats[2].base_stat;
-    console.log(`${pokemon1.name} has ${pokemon1.hp} hp, ${pokemon1.attack} attack and ${pokemon1.defense} defense`);
-    console.log(`${pokemon2.name} has ${pokemon2.hp} hp, ${pokemon2.attack} attack and ${pokemon2.defense} defense`);
+    console.debug(`${pokemon1.name} has ${pokemon1.hp} hp, ${pokemon1.attack} attack and ${pokemon1.defense} defense`);
+    console.debug(`${pokemon2.name} has ${pokemon2.hp} hp, ${pokemon2.attack} attack and ${pokemon2.defense} defense`);
     do {
-        console.log(`${pokemon1.name} has ${pokemon1.hp} hp left`);
-        console.log(`${pokemon2.name} has ${pokemon2.hp} hp left`);
+        console.debug(`${pokemon1.name} has ${pokemon1.hp} hp left`);
+        console.debug(`${pokemon2.name} has ${pokemon2.hp} hp left`);
         pokemon1.hp = calculateFight(pokemon1.hp, pokemon1.defense, pokemon2.attack);
-        // if (pokemon2.hp <= 0) {
-        //     console.log(`${pokemon1.name} won!`);
-        //     return 1;
-        // }
+        if (pokemon2.hp <= 0) {
+            console.log(`${pokemon1.name} won!`);
+            return 1;
+        }
         pokemon2.hp = calculateFight(pokemon2.hp, pokemon2.defense, pokemon1.attack);
-        // if (pokemon1.hp <= 0) {
-        //     console.log(`${pokemon2.name} won!`);
-        //     return 2;
-        // }
+        if (pokemon1.hp <= 0) {
+            console.log(`${pokemon2.name} won!`);
+            return 2;
+        }
     } while (pokemon1.hp > 0 && pokemon2.hp > 0);
 }
